7ffc599e367aa86604223d9f5ee9cf880db48d6b
Cody Moss <codytyler.moss@gmail.com>
2014-08-27 21:38:28 -0600

    trying to get new clients generated

--- scripts/logInCtrl.js
+++ scripts/logInCtrl.js
@@ -7,16 +7,33 @@ var app = angular.module("callCenter", ["firebase"]);
 app.factory("Profile", ["$firebase", function($firebase) {
   return function(company) {
     // create a reference to the user's profile
-    var ref = new Firebase("https://callcenter.firebaseio.com/<client name>/").child(company);
+    var ref = new Firebase("https://callcenter.firebaseio.com/company/leads/").child(company);
 
     // return it as a synchronized object
     return $firebase(ref).$asObject();
   }
 }]);
 
-app.controller("ProfileCtrl", ["$scope", "Profile",
-  function($scope, Profile) {
+app.controller("ProfileCtrl", ["$scope", '$firebase', "Profile",
+  function($scope, $firebase, Profile) {
+
+   
+
+
+    $scope.lead = 'asldjkf'
+   // var lead = $scope.profile
     // put our profile in the scope for use in DOM
-    $scope.profile = Profile('company contact');
+    $scope.profile = Profile($scope.lead);
+
+    
+
+    // var mainRef = new Firebase('https://callcenter.firebaseio.com/company/leads/');
+    // var thing = $firebase(mainRef).$asObject();
+    // thing.$loaded().then(function(data){
+    //   $scope.data = data;
+    // })
+
+
   }
 ]);
+
