33d06516943987bcc560c16fcd7201a40b59f422
Cody Moss <codytyler.moss@gmail.com>
2014-09-03 23:13:58 -0600

    got the automatic lead look up working.

--- scripts/apps.js
+++ scripts/apps.js
@@ -22,18 +22,17 @@ var callCenter = angular.module('callCenter', ["ui.router", "firebase", "ngGrid"
             templateUrl: "html/route2.html"
         })
         .state('route2.step1', {
-            url: "/route2/cheese",
+            url: "/route2/step1",
             templateUrl: "html/route2.step1.html"
         })
-             .state('route2.step2', {
-            url: "/route2/hi",
+        .state('route2.step2', {
+            url: "/route2/step2",
             templateUrl: "html/route2.step2.html"
         })
-              .state('home', {
+        .state('home', {
             url: "/home",
             templateUrl: "html/products.html"
         })
-
         .state('route3', {
             url: "/route3",
             templateUrl: "html/route3.html"
@@ -44,7 +43,8 @@ var callCenter = angular.module('callCenter', ["ui.router", "firebase", "ngGrid"
         })
          .state('leads', {
             url: "/leads",
-            templateUrl: "html/leads.html"
+            templateUrl: "html/leads.html",
+            controller: 'gridCtrl'
         })
          //  .state('products', {
          //    url: "/products",
@@ -59,122 +59,104 @@ var callCenter = angular.module('callCenter', ["ui.router", "firebase", "ngGrid"
         //   })
     });
 
-callCenter.controller("LeadsCtrl", function($scope, $firebase, $http) {
-  var ref = new Firebase("https://callcenter.firebaseio.com/Company/Leads");
-  var sync = $firebase(ref);
-  $scope.leads = sync.$asArray();
+      callCenter.controller("LeadsCtrl", function($scope, $firebase, $http, $rootScope) {
+        var ref = new Firebase("https://callcenter.firebaseio.com/Company/Leads");
+        var sync = $firebase(ref);
+        $scope.leads = sync.$asArray();
 
-  $scope.addLead = function(lead) {
-    $scope.leads.$add(lead);
-    
-  };
+        $scope.addLead = function(lead) {
+          $scope.leads.$add(lead);
+          
+        };
 
-  $http({
-    method: 'get',
-    url: '/getToken'
-  }).then(function(data){
-    twilioStuff(data.data);
-  }, function(data){
-    console.log('there was an error');
-  })
+        $http({
+          method: 'get',
+          url: '/getToken'
+        }).then(function(data){
+          twilioStuff(data.data);
+        }, function(data){
+          console.log('there was an error');
+        })
 
-  var twilioStuff = function(token){
-        Twilio.Device.setup(token);
-        // Register an event handler to be called when there is an incoming
-        // call:
+        var twilioStuff = function(token){
+              Twilio.Device.setup(token);
+              // Register an event handler to be called when there is an incoming
+              // call:
 
-        var connection=null;
+              var connection=null;
 
-        Twilio.Device.incoming(function (conn) {
-          if (confirm('Accept incoming call from ' + conn.parameters.From + '?')){
-            $scope.fromNumber = conn.parameters.From;
-            for(var i = 0; i < $scope.leads.length; i++){
-              if($scope.leads[i].phone === $scope.fromNumber){
-                console.log('Full Data is ', $scope.leads[i]);
-              } else {
-                console.log('That number is not found');
-              }
-            }
-            connection = conn;
-            conn.accept();
-          } else{
-            connection=conn;
-            conn.ignore();
-          }
-        });
+              Twilio.Device.incoming(function (conn) {
+                if (confirm('Accept incoming call from ' + conn.parameters.From + '?')){
+                  $scope.fromNumber = conn.parameters.From;
+                  for(var i = 0; i < $scope.leads.length; i++){
+                    if($scope.leads[i].phone === $scope.fromNumber){
+                       $rootScope.test= 'esfsefsefsef';
+                      // console.log('herereerere');
+                      // console.log('Full Data is ', $scope.leads[i]);
+                      $scope.$apply(function(){
+                        $scope.personCalling = $scope.leads[i];
+                      })
+                      
+                    } else {
+                      console.log('That number is not found');
+                    }
+                  }
+                  connection = conn;
+                  conn.accept();
+                } else{
+                  connection=conn;
+                  conn.ignore();
+                }
+              });
 
-        // Register an event handler for when a call ends for any reason
-        Twilio.Device.disconnect(function(connection) {
-          $('#hangup').click(function() {
-            Twilio.Device.disconnectAll();   
-          })
-        });
+              // Register an event handler for when a call ends for any reason
+              Twilio.Device.disconnect(function(connection) {
+                $('#hangup').click(function() {
+                  Twilio.Device.disconnectAll();   
+                })
+              });
 
-        $("#call").click(function() {  
-          params = { "tocall" : $('#tocall').val()};
-          connection =  Twilio.Device.connect({
-            CallerId:'+18012279533', // Replace this value with a verified Twilio number:
-                                  // https://www.twilio.com/user/account/phone-numbers/verified
-            PhoneNumber:$('.form-control').val() //pass in the value of the text field
-          });
-        });
+              $("#call").click(function() {  
+                params = { "tocall" : $('#tocall').val()};
+                connection =  Twilio.Device.connect({
+                  CallerId:'+18012279533', // Replace this value with a verified Twilio number:
+                                        // https://www.twilio.com/user/account/phone-numbers/verified
+                  PhoneNumber:$('.form-control').val() //pass in the value of the text field
+                });
+              });
 
-        $.each(['0','1','2','3','4','5','6','7','8','9','star','pound'], function(index, value) { 
-          $('#button' + value).click(function(){ 
-            if(connection) {
-                if (value=='star')
-                    connection.sendDigits('*')
-                else if (value=='pound')
-                    connection.sendDigits('#')
-                else
-                    connection.sendDigits(value)
-                return false;
-            } else {
-             $('.form-control').val($('.form-control').val() + value)
-            } 
-            });
-        });
-  }
-
-
-
-
-        // Set up the Twilio "Device" (think of it as the browser's phone) with
-        // our server-generated capability token, which will be inserted by the
-        // EJS template system
-
+              $.each(['0','1','2','3','4','5','6','7','8','9','star','pound'], function(index, value) { 
+                $('#button' + value).click(function(){ 
+                  if(connection) {
+                      if (value=='star')
+                          connection.sendDigits('*')
+                      else if (value=='pound')
+                          connection.sendDigits('#')
+                      else
+                          connection.sendDigits(value)
+                      return false;
+                  } else {
+                   $('.form-control').val($('.form-control').val() + value)
+                  } 
+                  });
+              });
+        }
 
 
+      });
 
+      callCenter.controller('gridCtrl', function($scope) {
+          $scope.gridOptions = { data: 'leads',
+            height: '110px',
+            sortInfo: {fields: ['Name', 'Phone', 'Location', 'notes'], directions: ['asc']},
+            columnDefs: [
+              {field: 'name', displayName: 'Name', width: '150px'},
+              {field: 'phone', displayName: 'Phone', width: '110px'},
+              {field: 'email', displayName: 'Email', width: '200px'},
+              {field: 'location', displayName: 'Location', width:'300px'},
+              {field: 'notes', displayName: 'Notes', width:'375px'},
+            ]
 
+      };
 
-
-
-
-
-
-});
-
-callCenter.controller('gridCtrl', function($scope) {
-    $scope.gridOptions = { data: 'leads',
-      height: '110px',
-      sortInfo: {fields: ['Name', 'Phone', 'Location', 'notes'], directions: ['asc']},
-      columnDefs: [
-        {field: 'name', displayName: 'Name', width: '150px'},
-        {field: 'phone', displayName: 'Phone', width: '110px'},
-        {field: 'email', displayName: 'Email', width: '200px'},
-        {field: 'location', displayName: 'Location', width:'300px'},
-        {field: 'notes', displayName: 'Notes', width:'375px'},
-      ]
-
-};
-
-});
-
-
-
-// [{name: "Moroni", age: 50},
-//                      {name: "Tiancum", age: 43},  
-//                      {name: "Jacob", age: 27},
-//                      {name: "Nephi", age: 29},
-//                      {name: "Enos", age: 34}];
+      });
